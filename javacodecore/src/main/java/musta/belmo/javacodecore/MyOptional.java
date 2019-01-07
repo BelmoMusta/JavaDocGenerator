682d0a46f84c49d40dac07f7bb440853279e9588
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2019-01-07 09:04:07 +0100

    adding optional

---
+++ javacodecore/src/main/java/musta/belmo/javacodecore/MyOptional.java
@@ -0,0 +1,94 @@
+package musta.belmo.javacodecore;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class MyOptional<T> {
+
+    private static final MyOptional<?> EMPTY = new MyOptional<>();
+    private final Optional<T> internalOptional;
+
+    private MyOptional() {
+        internalOptional = Optional.empty();
+    }
+
+    private MyOptional(T t) {
+        internalOptional = Optional.of(t);
+    }
+
+    public static <T> MyOptional<T> empty() {
+        return (MyOptional<T>) EMPTY;
+    }
+
+    public static <T> MyOptional<T> of(T value) {
+        return new MyOptional<>(value);
+    }
+
+    public static <T> MyOptional<T> ofNullable(T value) {
+        return value == null ? empty() : of(value);
+    }
+
+    public T get() {
+        return internalOptional.get();
+    }
+
+    public boolean isPresent() {
+        return internalOptional.isPresent();
+    }
+
+    public void ifPresent(Consumer<? super T> consumer) {
+        internalOptional.ifPresent(consumer);
+    }
+
+    public MyOptional<T> filter(Predicate<? super T> predicate) {
+        if (isPresent()) {
+            return predicate.test(internalOptional.get()) ? this : empty();
+        }
+        return this;
+    }
+
+    public <U> MyOptional<U> map(Function<? super T, ? extends U> mapper) {
+        if (isPresent()) {
+            return empty();
+        }
+        return ofNullable(mapper.apply(internalOptional.orElse(null)));
+    }
+
+    public <U> MyOptional<U> flatMap(Function<? super T, MyOptional<U>> mapper) {
+
+        Objects.requireNonNull(mapper);
+        if (!isPresent())
+            return empty();
+        else {
+            return Objects.requireNonNull(mapper.apply(internalOptional.orElse(null)));
+        }
+
+    }
+
+    public T orElse(T other) {
+        return internalOptional.orElse(other);
+    }
+
+    public T orElseGet(Supplier<? extends T> other) {
+        return internalOptional.orElseGet(other);
+    }
+
+    public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
+        return internalOptional.orElseThrow(exceptionSupplier);
+    }
+    //
+
+    public T orElseIfPredicate(T other, Predicate<T> predicate) {
+        if (isPresent()) {
+            final T t = get();
+            if (!predicate.test(t)) {
+                return t;
+            }
+        }
+        return other;
+    }
+}
--- mappingGenerator/src/main/java/musta/belmo/mappinggenerator/service/InterfaceDeriver.java
+++ mappingGenerator/src/main/java/musta/belmo/mappinggenerator/service/InterfaceDeriver.java
@@ -5,6 +5,10 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import musta.belmo.javacodecore.CodeUtils;
+import musta.belmo.javacodecore.MyOptional;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Optional;
 
 public class InterfaceDeriver {
 
@@ -14,6 +18,7 @@ public class InterfaceDeriver {
 
         for (ClassOrInterfaceDeclaration classOrInterfaceDeclaration : lRet.findAll(ClassOrInterfaceDeclaration.class)) {
             classOrInterfaceDeclaration.setInterface(true);
+            classOrInterfaceDeclaration.setName(MyOptional.of(interfaceName).orElseIfPredicate("UnnamedInterface", StringUtils::isEmpty));
             for (MethodDeclaration methodDeclaration : classOrInterfaceDeclaration.findAll(MethodDeclaration.class)) {
                 methodDeclaration.setBody(null);
                 methodDeclaration.setPublic(false);
@@ -25,6 +30,6 @@ public class InterfaceDeriver {
 
     public CompilationUnit deriveInterfaceFromClass(String src, String interfaceName) {
 
-        return deriveInterfaceFromClass(JavaParser.parse(src),interfaceName);
+        return deriveInterfaceFromClass(JavaParser.parse(src), interfaceName);
     }
 }
